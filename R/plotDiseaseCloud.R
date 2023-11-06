af268bcca2af6a8d4088b462228a653c8ef4c2a0
Angela Uzelac <angela.uzelac@mail.utoronto.ca>
2023-11-06 01:14:53 -0500

    suppressWarnings in wordcloud filtering, use wordcloud2

--- R/plotDiseaseCloud.R
+++ R/plotDiseaseCloud.R
@@ -1,21 +1,29 @@
 
 plotDiseaseCloud <- function(disease_assoc_tbl){
 
-  disease_assoc_tbl <- gene_disease_assoc
-
   library(tm)
-  corpus <- tm::Corpus(tm::VectorSource(disease_assoc_tbl$DiseaseName))
-  corpus <- tm_map(corpus, content_transformer(tolower))
-  corpus <- tm_map(corpus, removePunctuation)
-  corpus <- tm_map(corpus, removeNumbers)
-  corpus <- tm_map(corpus, removeWords, stopwords("english"))
-  corpus <- tm_map(corpus, stripWhitespace)
-  # remove words like disorder, disorders
+  corpus <- Corpus(VectorSource(disease_assoc_tbl$DiseaseName))
+  suppressWarnings({
+    corpus <- tm_map(corpus, content_transformer(tolower))
+    corpus <- tm_map(corpus, removePunctuation)
+    corpus <- tm_map(corpus, removeNumbers)
+    corpus <- tm_map(corpus, removeWords, stopwords("english"))
+    corpus <- tm_map(corpus, stripWhitespace)
+    corpus <- tm_map(corpus, removeWords, c("disorder", "disorders", "related", "state", "use", "related", "major", "symptom", "symptoms"))
+    corpus <- tm_map(corpus,
+                     replace_word <- function(x) {
+                                        x <- gsub("abuse", "substance-abuse" , x)
+                                        return(x)
+                                      }
+                     )
+  })
 
   tdm <- TermDocumentMatrix(corpus)
   word_freq <- rowSums(as.matrix(tdm))
+  word_freq_df <- data.frame(word = names(word_freq), freq = word_freq)
 
-  wordcloud::wordcloud(names(word_freq), word_freq)
-
+  wordcloud2::wordcloud2(word_freq_df)
+  # when I make it smaller schizophrenia appears
+  # need to figure out how to normalize these results
 
 }
