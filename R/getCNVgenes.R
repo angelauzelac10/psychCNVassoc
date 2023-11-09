e61df89b0461ae428f174b0704d8d698a17a0ba4
Angela Uzelac <angela.uzelac@mail.utoronto.ca>
2023-11-08 23:19:12 -0500

    move CNV call validation to separate function. Dynamic histogram title in plotCNVsize

--- R/getCNVgenes.R
+++ R/getCNVgenes.R
@@ -72,30 +72,7 @@
 
 getCNVgenes <- function(CNV_call, chromosome_number = NULL, reference_genome = "GRCh38", show_piechart = FALSE){
 
-  if(!is.data.frame(CNV_call)){
-    stop("First argument passed into function getCNVgenes() must be a dataframe.")
-  }
-  if(nrow(CNV_call) < 1){
-    stop("There are no rows in the input CNV call dataframe.")
-  }
-  if(ncol(CNV_call) != 4){
-    stop("The CNV call dataframe must have 4 columns.")
-  }
-  if(!all(colnames(CNV_call) %in% c("chromosome_name", "start_position", "end_position", "type"))){
-    stop("The CNV call dataframe must have columns named chromosome_name, start_position, end_position, and type in that order.")
-  }
-  if(!all(CNV_call$chromosome_name %in% c(1:22, "X", "Y"))){
-    stop("Chromosome number must be 1-22, X, or Y.")
-  }
-  if(!all(is.numeric(CNV_call$start_position)) | !all(is.numeric(CNV_call$end_position))){
-    stop("Start and end position must be integer values.")
-  }
-  if(!all(CNV_call$start_position < CNV_call$end_position)){
-    stop("The start position must be before the end position for each CNV.")
-  }
-  if(!all(CNV_call$type %in% c("DEL", "DUP"))){
-    stop("The value of the CNV type must be either 'DUP' or 'DEL'.")
-  }
+  validateCNVcall(CNV_call)
 
   if(!is.null(chromosome_number)){
     CNV_call <- CNV_call[CNV_call$chromosome_name == chromosome_number, ]
--- R/plotSummary.R
+++ R/plotSummary.R
@@ -3,50 +3,29 @@
 
 plotCNVsize <- function(CNV_call, chromosome_number = NULL){
 
-  if(!is.data.frame(CNV_call)){
-    stop("First argument passed into function plotCNVsize() must be a dataframe.")
-  }
-  if(nrow(CNV_call) < 1){
-    stop("There are no rows in the input CNV call dataframe.")
-  }
-  if(ncol(CNV_call) != 4){
-    stop("The CNV call dataframe must have 4 columns.")
-  }
-  if(!all(colnames(CNV_call) %in% c("chromosome_name", "start_position", "end_position", "type"))){
-    stop("The CNV call dataframe must have columns named chromosome_name, start_position, end_position, and type in that order.")
-  }
-  if(!all(CNV_call$chromosome_name %in% c(1:22, "X", "Y"))){
-    stop("Chromosome number must be 1-22, X, or Y.")
-  }
-  if(!all(is.numeric(CNV_call$start_position)) | !all(is.numeric(CNV_call$end_position))){
-    stop("Start and end position must be integer values.")
-  }
-  if(!all(CNV_call$start_position < CNV_call$end_position)){
-    stop("The start position must be before the end position for each CNV.")
-  }
-  if(!all(CNV_call$type %in% c("DEL", "DUP"))){
-    stop("The value of the CNV type must be either 'DUP' or 'DEL'.")
-  }
+  validateCNVcall(CNV_call)
+
+  hist_title <- "Distribution of CNV Sizes"
 
   if(!is.null(chromosome_number)){
     CNV_call <- CNV_call[CNV_call$chromosome_name == chromosome_number, ]
+    hist_title <- paste0(hist_title, " (Chromosome ", chromosome_number, ")", sep = "")
   }
 
-
-  cnv_data$size <- cnv_data$end_position - cnv_data$start_position
+  CNV_call$size <- CNV_call$end_position - CNV_call$start_position
 
   cnv_size_hist <- ggplot() +
-    geom_histogram(cnv_data[cnv_data$type == 'DEL', ],
+    geom_histogram(CNV_call[CNV_call$type == 'DEL', ],
                    mapping=aes(x = size, fill = "del"),
                    color = "black",
                    position = "stack") +
     scale_x_log10() +
-    geom_histogram(cnv_data[cnv_data$type == 'DUP', ],
+    geom_histogram(CNV_call[CNV_call$type == 'DUP', ],
                    mapping = aes(x = size, fill = "dup"),
                    color = "black",
                    position = "stack") +
     theme_bw() +
-    ggtitle('Distribution of CNV Sizes') +
+    ggtitle(hist_title) +
     xlab("Log-Size of CNV") +
     ylab("Number of CNVs") +
     scale_fill_manual(name = NULL,
