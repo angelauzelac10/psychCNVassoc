5588f5e399024dde0633a39cb7f4378f7bbb2c91
Angela Uzelac <angela.uzelac@mail.utoronto.ca>
2023-11-12 00:19:47 -0500

    add comments and clean up

--- R/getDiseaseAssoc.R
+++ R/getDiseaseAssoc.R
@@ -58,23 +58,25 @@
 
 getDiseaseAssoc <- function(gene_list){
 
+  # validate input list of genes
   if(!is.vector(gene_list) | !(typeof(gene_list) == "character")){
     stop("The argument passed into function getDiseaseAssoc() must be a character vector.")
   }
 
+  # get gene-disease associations
   psy_gene_disease <- psygenet2r::psygenetGene(gene = gene_list,
                                                database = "ALL",
                                                evidenceIndex = c(">", 0),
                                                verbose = FALSE,
                                                warnings = FALSE)
-
   psy_gene_disease_tbl <- psy_gene_disease@qresult
+  # edit column names
   current_colnames <- colnames(psy_gene_disease_tbl)
   new_colnames <- substr(current_colnames, 4, nchar(current_colnames))
   colnames(psy_gene_disease_tbl) <- new_colnames
 
+  # verify number of genes not found in the PsyGeNet database and display warning message
   gene_diff <- gene_list[!(unique(gene_list) %in% unique(psy_gene_disease_tbl$Gene_Symbol))]
-
   if(length(gene_diff) > 0){
     warning_message <- paste(length(gene_diff), "out of", length(gene_list), "of the given genes are not in PsyGeNET. Both psycur15 and psycur16 databases were queried.")
     warning(warning_message)
