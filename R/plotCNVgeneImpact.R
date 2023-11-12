19e4f13358fafb6bee1a4b737ff6030a2a448d5e
Angela Uzelac <angela.uzelac@mail.utoronto.ca>
2023-11-12 00:20:09 -0500

    add comments and clean up

--- R/plotCNVgeneImpact.R
+++ R/plotCNVgeneImpact.R
@@ -9,6 +9,7 @@
 
 plotCNVgeneImpact <- function(genic_CNV_count, total_CNV_count){
 
+  # validate CNV counts
   if(genic_CNV_count < 1){
     stop("Could not produce piechart. There are no CNVs that contain genes.")
   }
@@ -16,22 +17,32 @@ plotCNVgeneImpact <- function(genic_CNV_count, total_CNV_count){
     stop("Could not produce piechart. There cannot be more CNVs that contain genes than there are CNVs in total.")
   }
 
+  # calculate number of non-genic CNVs
   nongenic_CNV_count <- total_CNV_count - genic_CNV_count
   counts <- c(genic_CNV_count, nongenic_CNV_count)
-  labels <- c(paste0(round(100 * genic_CNV_count/total_CNV_count, 1), "%"), paste0(round(100 * nongenic_CNV_count/total_CNV_count, 1), "%"))
 
+  # set labels (percent genic vs. non-genic out of total)
+  labels <- c(paste0(round(100 * genic_CNV_count/total_CNV_count, 1), "%"),
+              paste0(round(100 * nongenic_CNV_count/total_CNV_count, 1), "%"))
+
+  # capture default par
   current_mai <- par("mai")
+  # change margins
   par(mai = c(0.3, 0.3, 0.3, 0.3))
+  # plot piechart
   pie(counts,
       labels = labels,
       col = c("lightblue", "orangered"),
       main = "Distribution of Genic vs Non-Genic CNVs",
       radius = 0.8)
+  # add legend
   legend("topright",
          legend = c("CNVs containing 1 or more genes", "CNVs containing no genes"),
          fill = c("lightblue", "orangered"),
          cex = 0.6,
          bty = "n")
+
+  # return par to default state
   par(mai = current_mai)
 
 }
