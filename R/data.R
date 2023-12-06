0c7117055e405f3b82a577d9d75c61f95f6ac085
Angela Uzelac <angela.uzelac@mail.utoronto.ca>
2023-12-06 18:59:21 -0500

    adding details to sample dataset descriptions

--- R/data.R
+++ R/data.R
@@ -2,6 +2,9 @@
 #'
 #' A data frame containing a sample CNV call. Obtained from a sample CNV call
 #' dataset named ACMG_examples.hg19.bed from the ClassifyCNV package in the /Examples subdirectory.
+#' This dataset contains a subset of 21 CNV calls described by 4 fields: chromosome name, start position,
+#' end position, and type (deletion or duplication). The CNV calling was done on Homo sapiens genotype
+#' data using genome build hg19 and contain autosomes 1-5, 6, 11-3, 15, 17-19, 22, and sex chromosome X.
 #'
 #' @source Gurbich, T.A., Ilinsky, V.V. (2020). ClassifyCNV: a tool for clinical
 #' annotation of copy-number variants. Sci Rep 10, 20375. \href{https://doi.org/10.1038/s41598-020-76425-3}{Link}
@@ -24,6 +27,9 @@
 #'
 #' A data frame containing a random subset of a sample CNV call. Obtained from a sample CNV call
 #' dataset named 1000Genomes.hg38.bed from the ClassifyCNV package in the /Examples subdirectory.
+#' This dataset contains a subset of 10,000 CNV calls described by 4 fields: chromosome name, start position,
+#' end position, and type (deletion or duplication). The CNV calling was done on Homo sapiens genotype
+#' data using genome build hg38 and contain autosomes 1-22 and sex chromosome X.
 #'
 #' @source Gurbich T, Ilinsky V (2020). ClassifyCNV: a tool for clinical
 #' annotation of copy-number variants. Sci Rep 10, 20375. \href{https://doi.org/10.1038/s41598-020-76425-3}{Link}
